8380b7070217e4326d59e79a2f055a70f37c9158
Henry434ddd <cl@cs-MacBook-Air.local>
2023-02-26 17:37:53 -0800

    finish

---
+++ Counter.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.21 <0.9.0;
+
+contract Counter {
+
+     uint count;
+
+     event  Increment(uint value);
+     event  Decrement(uint value);
+
+     constructor() public {
+         count = 0;
+     }
+
+    function  increment() public{
+        count++;
+        emit Increment(count);
+     }
+
+     function decrement() public{
+         count--;
+         emit Decrement(count);
+     }
+
+     function read() view public returns(uint) {
+        return count;
+     }
+
+
+
+
+
+
+
+
+
+}
